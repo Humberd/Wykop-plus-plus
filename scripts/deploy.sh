4b44eb9e1f6fcd79855e34c51b3c1dd5b9ad838e
Humberd <humberd2@gmail.com>
2018-12-31 23:53:44 +0100

    v0.0.4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "${CLIENT_ID}"
+echo "${CLIENT_SECRET}"
+echo "${AUTH_CODE}"
+echo "${ACCESS_TOKEN}"
+
 ACCESS_TOKEN=$(curl "https://accounts.google.com/o/oauth2/token" -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${AUTH_CODE}&grant_type=authorization_code&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token) \
     && curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -X PUT -T ${BUILD_BUILDID}.zip -v "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APP_ID}" \
     && curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -H "Content-Length: 0" -X POST -v "https://www.googleapis.com/chromewebstore/v1.1/items/${APP_ID}/publish"
