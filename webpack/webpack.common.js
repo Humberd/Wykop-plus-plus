8086a66b21bdb074fa4db6228447736ddb00ec86
Humberd <humberd2@gmail.com>
2019-01-05 10:27:15 +0100

    Save commit

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,9 @@
 const Path = require('path');
+const Webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 module.exports = {
   entry: {
@@ -17,19 +20,20 @@ module.exports = {
     new CopyWebpackPlugin([
       {from: Path.resolve(__dirname, '../public'), to: '.'},
     ]),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new CheckerPlugin()
   ],
   resolve: {
     alias: {
       '~': Path.resolve(__dirname, '../src'),
     },
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
@@ -39,6 +43,19 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.s?css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.([tj])s?$/,
+        loader: ['awesome-typescript-loader'],
+        exclude: [/node_modules/],
+      }
     ],
   },
 };
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,7 +1,6 @@
 const Path = require('path');
 const Webpack = require('webpack');
 const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -19,27 +18,6 @@ module.exports = merge(common, {
   plugins: [
     new Webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new Webpack.optimize.ModuleConcatenationPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css',
-    }),
+    })
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.s?css/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
 });
