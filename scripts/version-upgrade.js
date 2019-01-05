7d3e827aa4b56dbfb1cd730abe92b3cbe011244a
Humberd <humberd2@gmail.com>
2019-01-05 23:27:30 +0100

    Added manifest upgrade script

--- scripts/version-upgrade.js
+++ scripts/version-upgrade.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 const currentVersion = require('../package.json').version;
 
-const manifest = require('../public/manifest');
+const manifest = require('../public/manifest.json');
 
 manifest.version = currentVersion;
 
-fs.writeFileSync('../public/manifest', manifest,{});
+fs.writeFileSync('../public/manifest.json', manifest,{});
