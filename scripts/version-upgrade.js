9de4fa5e22d2906dc8c732e58b4629258a6a8c69
Humberd <humberd2@gmail.com>
2019-01-05 23:50:22 +0100

    1.1.27

--- scripts/version-upgrade.js
+++ scripts/version-upgrade.js
@@ -1,19 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log(__dirname);
-
 const currentVersion = require('../package.json').version;
 const manifest = require('../public/manifest.json');
 
 manifest.version = currentVersion;
 
-console.log(manifest);
-console.log(__dirname);
-
-
-const foo = path.resolve(__dirname, '../public/manifest.json');
-
-console.log(foo);
-fs.writeFileSync('./manifest.json', JSON.stringify(manifest),
+fs.writeFileSync(path.resolve(__dirname, '../public/manifest.json'), JSON.stringify(manifest),
     {encoding: 'utf8'});
